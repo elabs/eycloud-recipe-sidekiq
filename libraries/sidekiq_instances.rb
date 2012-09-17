9e90779b338b625f29b29b23df022f053358933c
Jonas Nicklas and Kim Burgestrand <dev+jnicklas+burgestrand@elabs.se>
2012-09-17 13:27:37 +0200

    Use sidekiq for all instances

--- libraries/sidekiq_instances.rb
+++ libraries/sidekiq_instances.rb
@@ -2,6 +2,7 @@ class Chef
   class Recipe
     # Does this instance run sidekiq?
     def sidekiq_instance?
+      return true # for now, we use it everywhere
       role = node[:instance_role]
       role == 'solo' || role == 'eylocal' || (role == 'util' && node[:name] =~ /^sidekiq/)
     end
